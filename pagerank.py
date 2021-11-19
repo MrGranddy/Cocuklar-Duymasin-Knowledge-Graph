d73eb8a152c8de5faccac45d24e81bc5ffba2b90
MrGranddy <bugrayesilkaynak@gmail.com>
2021-11-19 11:33:11 +0100

    Better colors and label font

--- pagerank.py
+++ pagerank.py
@@ -14,14 +14,14 @@ rel = list(zip(source, target))
 G.add_edges_from( rel )
 
 node_list = list(G)
-node_sizes = [ ppr1[x] * 100000 for x in node_list ]
+node_sizes = [ ppr1[x] * 200000 for x in node_list ]
 pos = nx.kamada_kawai_layout(G)
 plt.figure(num=None, figsize=(40, 40), dpi=80)
 plt.axis('off')
 fig = plt.figure(1)
-nx.draw_networkx_nodes(G, pos, node_size=node_sizes)
+nx.draw_networkx_nodes(G, pos, node_size=node_sizes, node_color='#eb5c3f')
 nx.draw_networkx_edges(G, pos, edge_color=(0.9,0.9,0.9))
-nx.draw_networkx_labels(G, pos, font_size=12)
+nx.draw_networkx_labels(G, pos, font_size=18, font_weight='bold')
 
 plt.savefig("graph.pdf", bbox_inches="tight")
 pylab.close()
